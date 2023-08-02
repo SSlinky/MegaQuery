8673979ecd23dac0795f1219f7305cbecf0ab790
Sam Vanderslink <svanderslink@hotmail.com>
2023-08-02 16:20:43 +0800

    New ListIsDistinct function

---
+++ Utilities/Validators/ListIsDistinct.m
@@ -0,0 +1,15 @@
+// Copyright (C) 2023 Samuel Vanderslink
+// Permission granted under the MIT Open Source licence.
+// The above copyright notice shall be included in all
+// copies or substantial portions of the Software.
+
+(lst as list) as logical =>
+// Validates the list has unique values.
+//
+// Args:
+//    lst: the list to validate.
+//
+// Returns:
+//    true if the list does not contain duplicates.
+
+    List.Count(lst) - List.Count(List.Distinct(lst)) = 0
