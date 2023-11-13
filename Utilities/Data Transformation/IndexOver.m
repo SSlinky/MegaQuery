5d886d18a8a8214b0a76563e5cd2267dda79a39f
Sam <39886505+SSlinky@users.noreply.github.com>
2023-11-13 15:06:20 +0900

    Added comments and bug fixed

--- Utilities/Data Transformation/IndexOver.m
+++ Utilities/Data Transformation/IndexOver.m
@@ -1,12 +1,32 @@
-(tbl as table, over as text, newColName as text, startAt as number) as table =>
-let
-    #"Grouped Rows" = Table.Group(tbl, {over}, {{"Rows", each _, type table [Rule Name=text, Base Object=text, Block=number]}}),
-    #"Added Custom" = Table.AddColumn(#"Grouped Rows", "RowsIndexed", each Table.AddIndexColumn([Rows], newColName, startAt)),
+// Copyright (C) 2023 Samuel Vanderslink
+// Permission granted under the MIT Open Source licence.
+// The above copyright notice shall be included in all
+// copies or substantial portions of the Software.
 
-    #"Removed Other Columns" = Table.SelectColumns(#"Added Custom",{"RowsIndexed"}),
-    #"Expanded Index" = Table.ExpandTableColumn(
-        #"Removed Other Columns",
+(tbl as table, over as list, newColName as text, startAt as number) as table =>
+// Adds an incrementing index that resets over groups.
+//
+// Args:
+//    tbl: the table to index over a column.
+//    over: the names of the columns to group on.
+//    newColName: the name of the index column.
+//    startAt: the number of steps to evaluate.
+//
+// Returns:
+//    A table with child-parent steps unpivoted.
+//
+let
+    // Group and Index the rows.
+    #"Added Index Over Group" = Table.SelectColumns(
+        Table.AddColumn(
+            Table.Group(tbl, over, {{"Rows", each _, type table [tbl]}}),
+            "RowsIndexed", each Table.AddIndexColumn([Rows], newColName, startAt)),
+        {"RowsIndexed"}),
+
+    // Expand the groups.
+    #"Expanded Columns" = Table.ExpandTableColumn(
+        #"Added Index Over Group",
         "RowsIndexed",
-        Table.ColumnNames(Table.Transpose(#"Removed Other Columns")[Column1]{0}))
+        Table.ColumnNames(Table.Transpose(#"Added Index Over Group")[Column1]{0}))
 in
-    #"Expanded Index"
+    #"Expanded Columns"
