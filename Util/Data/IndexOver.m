e7729bb3646b7293eb54be94a89e819cbb282c92
Sam Vanderslink <sam.vanderslink@notis.net.au>
2024-01-16 07:25:33 +0800

    Preserve col types and optional sorting

--- Util/Data/IndexOver.m
+++ Util/Data/IndexOver.m
@@ -6,30 +6,59 @@
 // Namespace: /Util/Data
 // QueryName: IndexOver
 
-(tbl as table, over as list, newColName as text, startAt as number) as table =>
+(tbl as table, over as list, newColName as text,
+    optional startAt as number,
+    optional sortComparisonData as list) as table =>
 // Adds an incrementing index that resets over groups.
 //
 // Args:
 //    tbl: the table to index over a column.
 //    over: the names of the columns to group on.
 //    newColName: the name of the index column.
-//    startAt: the number of steps to evaluate.
+//    startAt: the index value to start at.
 //
 // Returns:
 //    A table with child-parent steps unpivoted.
 //
 let
-    // Group and Index the rows.
-    #"Added Index Over Group" = Table.SelectColumns(
-        Table.AddColumn(
-            Table.Group(tbl, over, {{"Rows", each _, type table [tbl]}}),
-            "RowsIndexed", each Table.AddIndexColumn([Rows], newColName, startAt)),
-        {"RowsIndexed"}),
+    // Set a unique column name.
+    tempCol = "hsjknbvlsjkbvubn",
 
-    // Expand the groups.
-    #"Expanded Columns" = Table.ExpandTableColumn(
-        #"Added Index Over Group",
-        "RowsIndexed",
-        Table.ColumnNames(Table.Transpose(#"Added Index Over Group")[Column1]{0}))
+    // Group the rows to index over.
+    Grouped = Table.SelectColumns(
+        Table.Group(tbl, over, {{tempCol, each _, type table [tbl]}}),
+        {tempCol}),
+
+    // Sort the rows if we have criteria.
+    Sorted = if sortComparisonData = null
+        then Grouped
+        else Table.TransformColumns(
+            Grouped, {{tempCol, each Table.Sort(_, sortComparisonData)}}),
+
+    // Add an index to each group.
+    Indexed = Table.TransformColumns(
+        Sorted,
+        {{
+            tempCol,
+            each Table.AddIndexColumn(
+                _,
+                newColName,
+                startAt)
+        }}),
+
+    // Expand the groups, using the first table in the first row as a ref.
+    Expanded = Table.ExpandTableColumn(
+        Indexed,
+        tempCol,
+        Table.ColumnNames(Table.Transpose(Indexed)[Column1]{0})),
+
+    // Reset column types.
+    ResetTypes = ApplyColumnTypes(
+        tbl, Table.TransformColumnTypes(Expanded,{{newColName, Int64.Type}})),
+
+    // Safely return the indexed table, or original table plus a faux index if we had nothing.
+    Result = if Table.RowCount(tbl) > 0
+        then ResetTypes
+        else Table.AddColumn(tbl, newColName, each null, Int64.Type)
 in
-    #"Expanded Columns"
+    Result
