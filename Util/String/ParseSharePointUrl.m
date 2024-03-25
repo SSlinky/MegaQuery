c2020f42e34161ade9190b01f24d586e5cc71059
Sam <39886505+SSlinky@users.noreply.github.com>
2024-03-25 21:42:03 +0800

    Now caters for direct file links
    Allows parsing of directories where the source is the full path from OneDrive, e.g., `CELL("filename") =  https://co.sharepoint.com/sites/CoTeam/Shared Documents/Path/[File.xlsx]Sheet1`

--- Util/String/ParseSharePointUrl.m
+++ Util/String/ParseSharePointUrl.m
@@ -18,61 +18,75 @@
 //    A list containing the Company, Team, and Folder Path.
 
 let
-    // Helper function to strip all args except RootFolder if it exists.
-    StripUrlArgs = (url as text) => let
-        base = Text.BeforeDelimiter(url, "?"),
-        path = Text.BetweenDelimiters(url, "RootFolder=", "&")
-        in if path = "" then base else base & "?RootFolder=" & path,
+    // Simplified method when a direct file reference is detected.
+    asFileUrl = (fileUrl as text) =>
+        {
+            Text.BetweenDelimiters(fileUrl, "//", "."),
+            Text.BetweenDelimiters(fileUrl, "sites/", "/"),
+            Text.BeforeDelimiter(fileUrl, "[")
+        },
 
-    // Helper function to parse the URL into its component parts.  
-    parseUrlStaging = (url as text) as table => let
-        // Create a table to work with the decoded URL.
-        workingTable = #table({"baseUrl"}, {{DecodeUrl(StripUrlArgs(url))}}),
+    // Longer method to parse a SharePoint directory path.
+    asFolderUrl = (folderUrl as text) => let
+        // Helper function to strip all args except RootFolder if it exists.
+        StripUrlArgs = (url as text) => let
+            base = Text.BeforeDelimiter(url, "?"),
+            path = Text.BetweenDelimiters(url, "RootFolder=", "&")
+            in if path = "" then base else base & "?RootFolder=" & path,
 
-        // Remove https:// from the start of the url.
-        removeProtocol = Table.TransformColumns(
-            workingTable,
-            {{"baseUrl", each Text.AfterDelimiter(_, "//"),
-            type text}}),
+        // Helper function to parse the URL into its component parts.  
+        parseUrlStaging = (url as text) as table => let
+            // Create a table to work with the decoded URL.
+            workingTable = #table({"baseUrl"}, {{DecodeUrl(StripUrlArgs(url))}}),
 
-        // Split the URL and extract company.
-        parseCompanyId = Table.TransformColumns(
-            Table.SplitColumn(
-                removeProtocol,
-                "baseUrl",
-                Splitter.SplitTextByEachDelimiter({"sites/"}, QuoteStyle.Csv, false),
-                {"company", "path"}),
-            {{"company", each Text.BeforeDelimiter(_, "."),
-            type text}}),
+            // Remove https:// from the start of the url.
+            removeProtocol = Table.TransformColumns(
+                workingTable,
+                {{"baseUrl", each Text.AfterDelimiter(_, "//"),
+                type text}}),
 
-        // Split the remainder into team and path.
-        parseTeam = Table.SplitColumn(
-            parseCompanyId,
-            "path",
-            Splitter.SplitTextByEachDelimiter({"/"}, QuoteStyle.Csv, false),
-            {"team", "path"}),
+            // Split the URL and extract company.
+            parseCompanyId = Table.TransformColumns(
+                Table.SplitColumn(
+                    removeProtocol,
+                    "baseUrl",
+                    Splitter.SplitTextByEachDelimiter({"sites/"}, QuoteStyle.Csv, false),
+                    {"company", "path"}),
+                {{"company", each Text.BeforeDelimiter(_, "."),
+                type text}}),
 
-        // Remove the team from the path.
-        parsePath = Table.TransformColumns(
-            parseTeam,
-            {{"path", each Text.AfterDelimiter(_, parseTeam[team]{0}),
-            type text}})
-    in parsePath,
+            // Split the remainder into team and path.
+            parseTeam = Table.SplitColumn(
+                parseCompanyId,
+                "path",
+                Splitter.SplitTextByEachDelimiter({"/"}, QuoteStyle.Csv, false),
+                {"team", "path"}),
 
-    // Parse the url into a staging table.
-    urlParts = parseUrlStaging(sharePointUrl),
+            // Remove the team from the path.
+            parsePath = Table.TransformColumns(
+                parseTeam,
+                {{"path", each Text.AfterDelimiter(_, parseTeam[team]{0}),
+                type text}})
+        in parsePath,
 
-    // Return the results list.
-    result = {
-        urlParts[company]{0},
-        urlParts[team]{0},
-        Text.Format(
-            "https://#{0}.sharepoint.com/sites/#{1}#{2}",
-            {
-                urlParts[company]{0},
-                urlParts[team]{0},
-                urlParts[path]{0} & "/"
-            })
-    }
+        // Parse the url into a staging table.
+        urlParts = parseUrlStaging(sharePointUrl),
+
+        // Return the results list.
+        result = {
+            urlParts[company]{0},
+            urlParts[team]{0},
+            Text.Format(
+                "https://#{0}.sharepoint.com/sites/#{1}#{2}",
+                {
+                    urlParts[company]{0},
+                    urlParts[team]{0},
+                    urlParts[path]{0} & "/"
+                })
+        }
+    in
+        result
 in
-    result
+    if Text.Contains(sharePointUrl, "[")
+    then asFileUrl(sharePointUrl)
+    else asFolderUrl(sharePointUrl)
